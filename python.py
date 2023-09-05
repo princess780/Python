d4de7335212f5c64cb0251e5dec14f0b1ba7c44d
princess780 <princessedeh820@gmail.com>
2023-09-05 15:54:35 +0100

    Class work

---
+++ python.py
@@ -0,0 +1,24 @@
+num1,num2 =input("Enter two number:").split()
+num1 = int(num1)
+num2 = int(num2)
+sum = num1 + num2
+difference = num1 - num2
+Product = num1 * num2
+Quotient = num1 / num2
+Remainder = num1 % num2
+
+print("{} + {} = {}". format(num1, num2, sum))
+print("{} - {} ={}". format(num1, num2, difference))
+print("{} * {} = {}". format(num1, num2, Product))
+print("{} / {} = {}". format(num1, num2, Quotient))
+print("{} % {} = {}". format(num1, num2, Remainder))
+
+num1, operator, num2 = input("Enter a mathematical opertion: ").split()
+num1 = int(num1)
+num2 = int(num2)
+
+
+#List of 
+# my_Objects = ("Chair", "Fan", "Television", "table", "Cal")
+# for Z in my_Objects:
+# print(Z)
